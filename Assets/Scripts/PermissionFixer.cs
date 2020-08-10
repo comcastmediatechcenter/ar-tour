61b70db940d731dce20c416e5ff3918f3e37cda2
Ryan Buxton <rbuxton1@gmail.com>
2020-08-10 10:03:25 -0600

    Player pin plus some extras

---
+++ Assets/Scripts/PermissionFixer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PermissionFixer : MonoBehaviour{
+
+  /*
+   *  Hopefully this will fix the Android resource request problem. The #if is
+   * a pre process directive to check if we are running on Android and if we are
+   * to include the script bellow. I have reason to believe it would not enjoy
+   * this additional code on iOS.
+   */
+  void Start(){
+    #if PLATFORM_ANDROID
+    if(!Permission.HasUserAuthorizedPermission(Permission.Camera)){
+      Permission.RequestUserPermission(Permission.Camera);
+    }
+    if(!Permission.HasUserAuthorizedPermission(Permission.FineLocation)){
+      Permission.RequestUserPermission(Permission.FineLocation);
+    }
+    if(!Permission.HasUserAuthorizedPermission(Permission.CoarseLocation)){
+      Permission.RequestUserPermission(Permission.CoarseLocation);
+    }
+    #endif
+  }
+
+  // Update is called once per frame
+  void Update(){
+
+  }
+}
